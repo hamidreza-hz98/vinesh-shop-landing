25ef1d9302cbdb5dcf63a3d723c4929f95134424
Hamidreza Hassanzadeh <hamidrezahz1999@gmail.com>
2025-08-21 18:31:49 +0330

    wishlist segment tasks

--- components/wrappers/profile/ProfileWishlistPageWrapper.js
+++ components/wrappers/profile/ProfileWishlistPageWrapper.js
@@ -1,8 +1,21 @@
+import PrimaryProductCard from '@/components/common/cards/PrimaryProductCard'
+import Pagination from '@/components/common/Pagination'
+import { products } from '@/constants/product'
 import React from 'react'
 
 const ProfileWishlistPageWrapper = () => {
   return (
-    <div>ProfileWishlistPageWrapper</div>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+      {
+        products.slice(0, 6).map((product, index) => 
+        <PrimaryProductCard key={index} product={product} />
+      )
+      }
+
+      <div className='col-span-full'>
+        <Pagination initialPage={1} initialPageSize={6} totalItems={31} />
+      </div>
+    </div>
   )
 }
 
